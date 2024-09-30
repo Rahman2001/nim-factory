9e3600decd7e30a61ceb1b293e3bb977669d98c4
Rahman <rahmanrejepov777@gmail.com>
2024-10-01 01:12:15 +0300

    Modified postBuild.bash
    - Modified files: postBuild.bash

--- postBuild.bash
+++ postBuild.bash
@@ -3,7 +3,3 @@
 # after all system packages and programming language specific package have been installed.
 #
 # Note: This file may be removed if you don't need to use it
-sudo apt-get update && apt-get -y install python3.10 python3-pip
-apt install unzip
-apt-get wget
-sudo apt-get update && apt-get -y install openmpi-bin libopenmpi-dev git git-lfs
