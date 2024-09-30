4da2409e431ebd8d301393343e4b8444bd576e08
Rahman <74003007+Rahman2001@users.noreply.github.com>
2024-09-30 23:34:02 +0300

    Update preBuild.bash

--- preBuild.bash
+++ preBuild.bash
@@ -3,4 +3,4 @@
 # before any system packages or programming language specific package have been installed.
 #
 # Note: This file may be removed if you don't need to use it
-apt-get update && apt-get -y install python3.10 python3-pip
+-root apt-get update && apt-get -y install python3.10 python3-pip
