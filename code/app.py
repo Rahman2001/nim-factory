b881561007b6f770504ed6da625296667ec0d760
Rahman <rahmanrejepov777@gmail.com>
2024-11-07 19:57:23 +0300

    Modified 2 files
    - Modified files: code/app.py, code/index.html

--- code/app.py
+++ code/app.py
@@ -1,12 +1,21 @@
 import gradio as gr
 import os
 
+script = '''
+function wrap() {
+    function test() {
+        document.getElementById('demo').innerHTML = "Hello"
+    }
+}
+'''
+    
 with open("index.html") as f:
     lines = f.readlines()
-
+    
 with gr.Blocks() as demo:   
     input_mic = gr.HTML(lines)
     out_text  = gr.Textbox()
+    demo.load(_js = script)
 
 if __name__ == "__main__":   
     proxy_prefix = os.environ.get("PROXY_PREFIX")
