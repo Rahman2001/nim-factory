807af90d08f5b31b9992bcc3097203ae709db936
Rahman <rahmanrejepov777@gmail.com>
2024-11-07 13:04:04 +0300

    Changed env w/ pip. Project Spec Modified & more...
    - Added pip packages: megatron-core,
    git+https://github.com/NVIDIA/NeMo.git, hydra-core, pytorch-lightning,
    braceexpand, webdataset, ijson, matplotlib, sacrebleu, Cython,
    pickleshare,
    tensorrt_llm-U--pre--extra-index-urlhttps://pypi.nvidia.com, gradio,
    Flask
    - Created app 'simple-gradio-app'
    - Added files: code/app.py
    - Modified files: postBuild.bash, requirements.txt

--- postBuild.bash
+++ postBuild.bash
@@ -3,17 +3,4 @@
 # after all system packages and programming language specific package have been installed.
 #
 # Note: This file may be removed if you don't need to use it
-pip3 install megatron-core
-pip3 install git+https://github.com/NVIDIA/NeMo.git
-pip3 install hydra-core
-pip3 install pytorch-lightning
-pip3 install braceexpand
-pip3 install webdataset
-pip3 install ijson
-pip3 install matplotlib
-pip3 install sacrebleu
-pip3 install Cython
-pip3 install pickleshare
-pip3 install Cython
-pip3 install tensorrt_llm -U --pre --extra-index-url https://pypi.nvidia.com
 git lfs install
