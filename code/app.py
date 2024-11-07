7c3e0d2e9d74bc28d5f0bd970f0ebd23dfa1785b
Rahman <rahmanrejepov777@gmail.com>
2024-11-07 15:43:22 +0300

    Modified code/app.py
    - Modified files: code/app.py

--- code/app.py
+++ code/app.py
@@ -1,16 +1,16 @@
-import gradio as gr
 import os
-
-def update(name):
-    return f"Welcome to Gradio, {name}!"
-
-with gr.Blocks() as demo:
-    gr.Markdown("Start typing below and then click **Run** to see the output.")
-    with gr.Row():
-        inp = gr.Textbox(placeholder="What is your name?")
-        out = gr.Textbox()
-    btn = gr.Button("Run")
-    btn.click(fn=update, inputs=inp, outputs=out)
+from flask import Flask
 
 proxy_prefix = os.environ.get("PROXY_PREFIX")
-demo.launch(server_name="0.0.0.0", server_port=8080, root_path=proxy_prefix)
+
+app = Flask(__name__)
+app.config['APPLICATION_ROOT'] = proxy_prefix
+app.config['SERVER_NAME'] = "0.0.0.0:8080"
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+    
+
+
+# demo.launch(server_name="0.0.0.0", server_port=8080, root_path=proxy_prefix)
