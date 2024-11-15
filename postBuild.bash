2169846669f4b7cff6096f81a9452d4d0f456612
Rahman Rejepov <rahmanrejepov777@gmail.com>
2024-11-16 01:54:24 +0300

    Modified postBuild.bash
    - Modified files: postBuild.bash

--- postBuild.bash
+++ postBuild.bash
@@ -5,6 +5,6 @@
 # Note: This file may be removed if you don't need to use it
 git lfs install
 pip3 install tensorrt_llm==0.14.0 -U --extra-index-url https://pypi.nvidia.com
-pip3 install tensorrt-cu12-libs==10.6.0
-pip3 install tensorrt-cu12-bindings==10.6.0
-pip3 install "nvidia-modelopt[all]" -U
+pip3 install tensorrt-cu12-libs==10.4.0
+pip3 install tensorrt-cu12-bindings==10.4.0
+pip3 install "nvidia-modelopt[all]"==0.17.0
