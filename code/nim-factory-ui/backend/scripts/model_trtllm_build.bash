990108749e4f54a4a33cea1ee276751a363777e8
Rahman <rahmanrejepov777@gmail.com>
2024-11-15 21:30:58 +0300

    Added code/nim-factory-ui/backend/scripts/model_trtllm_build.bash, Modified code/TensorRT-LLM
    - Added files:
    code/nim-factory-ui/backend/scripts/model_trtllm_build.bash
    - Modified files: code/TensorRT-LLM

---
+++ code/nim-factory-ui/backend/scripts/model_trtllm_build.bash
@@ -0,0 +1,22 @@
+cd "$PWD"
+cd ../../../models
+
+inputs=("$@")
+checkpoint_name="${inputs[0]}"
+output_dir="trtllm_""${checkpoint_name}"
+
+outcome=$(find ./"${output_dir}" -maxdepth 1 -mindepth 1 -type d)
+
+if [[ -z "$outcome" ]]; then
+  outcome=$(eval "trtllm-build --checkpoint_dir=./""${checkpoint_name}"" ""${inputs[*]}"" --output_dir=./trtllm_""${output_dir}")
+
+  if [[ "$outcome" -eq 0 ]]; then
+    echo "------------- TensorRT-LLM engine successfully built! ------------------------ "
+    else
+      echo "ERROR: Failed to build engine ... "
+      exit 128
+  fi
+
+  else
+    echo "INFO: TensorRT-LLM engine for that model already exists in that directory"
+fi
