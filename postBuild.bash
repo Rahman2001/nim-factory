e17b842e1802d41a93b2b5d1ca32bd3d37218852
Rahman <rahmanrejepov777@gmail.com>
2024-11-15 21:25:28 +0300

    Changed env w/ apt & pip. Modified 4 files
    - Added apt packages: python-is-python3; - Removed pip packages: Cython
    - Modified files: apt.txt, code/TensorRT-LLM, postBuild.bash,
    requirements.txt

--- postBuild.bash
+++ postBuild.bash
@@ -4,4 +4,7 @@
 #
 # Note: This file may be removed if you don't need to use it
 git lfs install
-pip3 install tensorrt_llm -U --pre --extra-index-url https://pypi.nvidia.com
+pip3 install tensorrt_llm==0.14.0 -U --extra-index-url https://pypi.nvidia.com
+pip3 install tensorrt-cu12-libs==10.6.0
+pip3 install tensorrt-cu12-bindings==10.6.0
+pip3 install "nvidia-modelopt[all]" -U
